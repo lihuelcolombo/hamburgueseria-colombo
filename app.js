eb7d6dfbc78a5aa093be1873b195bf466c0bba96
lihuelcolombo <lihuel.colombo@gmail.com>
2022-02-14 00:33:16 -0300

    Mejorado estilo del carrito

--- app.js
+++ app.js
@@ -17,17 +17,17 @@ function mostrarProductos(array){
         div.className = 'producto';
         div.innerHTML = `    <div class="row">
         <div class="col s12 m6">
-          <div class="card blue-grey darken-1">
-            <div class="card-content white-text">
-              <span class="card-title">${producto.nombre}</span>
-              <p>Se acompaña con nuestras famosas papas fritas!</p>
+            <div class="card blue-grey darken-1">
+                <div class="card-content white-text">
+                    <span class="card-title">${producto.nombre}</span>
+                    <p>Se acompaña con nuestras famosas papas fritas!</p>
+                    </div>
+                    <div class="card-action">
+                        <a id = "botonAgregar${producto.id}" class="btn-floating btn-large waves-effect waves-light green"><i class="material-icons">+</i></a>                  
+                    </div>
+                </div>
             </div>
-            <div class="card-action">
-                <a id = "botonAgregar${producto.id}" class="btn-floating btn-large waves-effect waves-light green"><i class="material-icons">+</i></a>                  
-            </div>
-          </div>
         </div>
-      </div>
     `
     contenedorProductos.appendChild(div);
 
